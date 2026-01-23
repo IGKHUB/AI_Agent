39a97de9d8c61b4be0c48743fff8ec52b2f5f7bc
IGKHUB <jevon2350@gmail.com>
2026-01-23 18:02:43 -0500

    added --verbose and added calc dir

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 load_dotenv()
@@ -10,20 +11,28 @@ client = genai.Client(api_key=api_key)
 
 parser = argparse.ArgumentParser(description="AI Agent")
 parser.add_argument("user_prompt", type=str, help="The prompt to send to the AI agent")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 args = parser.parse_args()
+
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
 response = client.models.generate_content(
     model="gemini-2.5-flash",
-    contents=args.user_prompt,
+    contents=messages,
 )
 usage_metadata = response.usage_metadata
 
+
 def main():
     if usage_metadata is not None:
-        print("Hello from ai-agent!")
-        print(f"Prompt tokens: {usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {usage_metadata.candidates_token_count}")
-        print(f"Response:\n{response.text}")
+        if args.verbose:
+            print("Hello from ai-agent!")
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {usage_metadata.candidates_token_count}")
+            print(f"Response:\n{response.text}")
+        else:
+            print(f"Response:\n{response.text}")
     else:
         raise RuntimeError("failed API request")
 
